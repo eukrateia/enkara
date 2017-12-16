3e5391e695baab996ade817321b74b235eecbe4d
David Demers <dave.demers@gmail.com>
2017-12-16 11:00:28 -0500

    update getFormData()

--- public/tc-app/tc-app.js
+++ public/tc-app/tc-app.js
@@ -6,9 +6,8 @@ var printPre = {
     var objArr = [];
 
     $('div[data-input-type]').each(function() {
-      var obj = null;
-      var inType = $(this).attr('type');
       var inDataInputType = $(this).attr('data-input-type');
+      var zid = $(this).attr('zid');
       switch(inDataInputType) {
         case 'text':
         case 'time':
@@ -16,24 +15,15 @@ var printPre = {
         case 'month':
         case 'number':
         case 'tel':
-          obj = {
-            inType: $(this).attr('type'),
-            inDataInputType: $(this).attr('data-input-type'),
-            id: $(this).attr('id') ? $(this).attr('id') : '',
-            name: $(this).attr('name') ? $(this).attr('name') : '',
-            value: $(this).text()
-          }
+          objArr[zid] = $(this).text();
           break;
         case 'radio':
+          var inName = $(this).attr('name');
+          objArr[zid] = $('input[name="' + inName + '"]').val();
+          break;
         case 'checkbox':
           var inName = $(this).attr('name');
-          obj = {
-            inType: $(this).attr('type'),
-            inDataInputType: $(this).attr('data-input-type'),
-            id: $(this).attr('id') ? $(this).attr('id') : '',
-            name: $(this).attr('name') ? $(this).attr('name') : '',
-            value: $('input[name=' + inName + ']:checked').val() ? $('input[name=' + inName + ']:checked').val() : ''
-          }
+          objArr[zid] = $(this).val();
           break;
         default:
           break;
@@ -42,7 +32,40 @@ var printPre = {
         objArr.push(obj);
       }
     });
-    console.log('*********** objArr ***********');
     console.log(objArr);
   }
+}
+
+
+//***** API insertRecords example *****
+//https://crm.zoho.com/crm/private/xml/Leads/insertRecords?newFormat=1&authtoken=a5645229e18b49eb2927c290aeed90ac&scope=crmapi&xmlData=<Leads><row no="1"><FL val="Lead Source">Web Download</FL><FL val="Company">Your Company</FL><FL val="First Name">David</FL><FL val="Last Name">Demers</FL><FL val="Email">testing@testing.com</FL><FL val="Title">Manager</FL><FL val="Phone">1234567890</FL><FL val="Home Phone">0987654321</FL><FL val="Other Phone">1212211212</FL><FL val="Fax">02927272626</FL><FL val="Mobile">292827622</FL></row></Leads>
+
+
+var zohoMap = {
+  "Lead Owner"        : "Lead Owner",
+  "Salutation"        : "Salutation",
+  "First Name"        : "First Name",
+  "Title"             : "Title",
+  "Last Name*"        : "Last Name*",
+  "Company*"          : "Company*",
+  "Lead Source"       : "Lead Source",
+  "Industry"          : "Industry",
+  "Annual Revenue"    : "Annual Revenue",
+  "Phone"             : "Phone",
+  "Mobile"            : "Mobile",
+  "Fax"               : "Fax",
+  "Email"             : "Email",
+  "Secondary Email"   : "Secondary Email",
+  "Skype ID"          : "Skype ID",
+  "Website"           : "Website",
+  "Lead Status"       : "Lead Status",
+  "Rating"            : "Rating",
+  "No. of Employees"  : "No. of Employees",
+  "Email Opt-out"     : "Email Opt-out",
+  "Street"            : "Street",
+  "City"              : "City",
+  "State"             : "State",
+  "Zip Code"          : "Zip Code",
+  "Country"           : "Country",
+  "Description"       : "Description",
 }
